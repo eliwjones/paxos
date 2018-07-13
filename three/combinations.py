a842480ee9201f9ccd0a33221e4dd0557b9ad2e3
eli <eli.jones@gmail.com>
2018-07-13 17:41:29 -0400

    problem three: less convoluted conversion of binary to tuple of 0s and 1s.

--- three/combinations.py
+++ three/combinations.py
@@ -1,9 +1,9 @@
 import sys
 
 
-def bin_to_list(n, padding=0):
-    s = bin(n)[2:].zfill(padding)
-    return [d for d in s]
+def bin_to_tuple(n, padding=0):
+    s = format(n, "0%sb" % (padding))
+    return tuple(s)
 
 
 given_string = sys.argv[1]
@@ -12,4 +12,4 @@ x_count = given_string.count('X')
 prepped_string = given_string.replace('X', '%s')
 
 for i in range(2 ** x_count):
-    print(prepped_string % tuple(bin_to_list(i, x_count)))
+    print(prepped_string % bin_to_tuple(i, x_count))
