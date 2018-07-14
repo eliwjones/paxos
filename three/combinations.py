2fe75df1ab2f356aedb1c4a8d809022498f19261
eli <eli.jones@gmail.com>
2018-07-13 20:48:36 -0400

    problem three: preparing to write tests.

--- three/combinations.py
+++ three/combinations.py
@@ -1,15 +1,29 @@
 import sys
 
+"""
+ Python 3 does not use xrange() so use range()
+
+ I want it to be explicit that we are not generating all 2^n ints at once whether
+ we are in Python 2 or 3.
+"""
+if sys.version_info[0] == 3:
+    xrange = range
+
 
 def bin_to_tuple(n, padding=0):
     s = format(n, "0%sb" % (padding))
     return tuple(s)
 
 
-given_string = sys.argv[1]
-x_count = given_string.count('X')
+def generate_combinations(given_string):
+    x_count = given_string.count('X')
+    prepped_string = given_string.replace('X', '%s')
 
-prepped_string = given_string.replace('X', '%s')
+    for i in xrange(2 ** x_count):
+        yield prepped_string % bin_to_tuple(i, x_count)
 
-for i in range(2 ** x_count):
-    print(prepped_string % bin_to_tuple(i, x_count))
+
+if __name__ == "__main__":
+    given_string = sys.argv[1]
+    for result in generate_combinations(given_string):
+        print(result)
