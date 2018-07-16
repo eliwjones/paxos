af2529e17f2ea3aad2f9f26b0a6bc42601d3b82b
eli <eli.jones@gmail.com>
2018-07-16 17:25:31 -0400

    problem one: message_hasher_test - tests still mostly smokey.. but maybe a little more clear about my intent to not overdo it.

--- one/message_hasher_test.go
+++ one/message_hasher_test.go
@@ -2,11 +2,73 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http/httptest"
-	"strings"
 	"testing"
 )
+
+func TestPostAndGetHandlersValidPayload(t *testing.T) {
+	/* POST a valid message to the postHandler(). */
+
+	payload := []byte(`{"message": "foo"}`)
+	req := httptest.NewRequest("POST", "/messages", bytes.NewReader(payload))
+	w := httptest.NewRecorder()
+
+	postHandler(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	strBody := string(body)
+
+	expectedHash := "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
+	expectedBody := fmt.Sprintf("{\n  \"digest\": \"%s\"\n}\n", expectedHash)
+
+	if strBody != expectedBody {
+		t.Errorf("Expected: '%s'\nGot: %s", expectedBody, strBody)
+	}
+
+	/* Now, lets see if we can GET our message back by its hash. */
+
+	req = httptest.NewRequest("GET", "/messages/"+expectedHash, nil)
+	w = httptest.NewRecorder()
+
+	getHandler(w, req)
+
+	resp = w.Result()
+	body, _ = ioutil.ReadAll(resp.Body)
+	strBody = string(body)
+
+	expectedBody = "{\n  \"message\": \"foo\"\n}\n"
+
+	if strBody != expectedBody {
+		t.Errorf("Expected: %s,\nGot: %s", strBody, expectedBody)
+	}
+}
+
+func TestPostHandlerError(t *testing.T) {
+	payload := []byte(`{"bad json`)
+	req := httptest.NewRequest("POST", "/messages", bytes.NewReader(payload))
+	w := httptest.NewRecorder()
+
+	postHandler(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	strBody := string(body)
+
+	expectedStatusCode := 400
+
+	if resp.StatusCode != expectedStatusCode {
+		t.Errorf("Expected StatusCode: %d, Got: %d", expectedStatusCode, resp.StatusCode)
+	}
+
+	expectedBody := "{\n  \"error\": \"Unmarshal Error: 'unexpected end of JSON input' with Body: '{\\\"bad json'\"\n}\n"
+
+	if strBody != expectedBody {
+		t.Errorf("Expected: '%s'\nGot: %s", expectedBody, strBody)
+	}
+}
 
 func TestGetHandlerNotFound(t *testing.T) {
 	req := httptest.NewRequest("GET", "/messages/abcdefg", nil)
@@ -24,38 +86,6 @@ func TestGetHandlerNotFound(t *testing.T) {
 	}
 
 	expectedBody := "{\n  \"error\": \"Message not found.\"\n}\n"
-
-	if string(body) != expectedBody {
-		t.Errorf("Expected: %s,\nGot: %s", string(body), expectedBody)
-	}
-}
-
-func TestPostHandler(t *testing.T) {
-	payload := []byte(`{"message": "foo"}`)
-	req := httptest.NewRequest("POST", "/messages", bytes.NewReader(payload))
-	w := httptest.NewRecorder()
-
-	postHandler(w, req)
-
-	resp := w.Result()
-	body, _ := ioutil.ReadAll(resp.Body)
-	strBody := string(body)
-
-	expectedHash := "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
-
-	if !strings.Contains(strBody, expectedHash) {
-		t.Errorf("Expected: '%s' in body.\n\nGot: %s", expectedHash, strBody)
-	}
-
-	req = httptest.NewRequest("GET", "/messages/"+expectedHash, nil)
-	w = httptest.NewRecorder()
-
-	getHandler(w, req)
-
-	resp = w.Result()
-	body, _ = ioutil.ReadAll(resp.Body)
-
-	expectedBody := "{\n  \"message\": \"foo\"\n}\n"
 
 	if string(body) != expectedBody {
 		t.Errorf("Expected: %s,\nGot: %s", string(body), expectedBody)
